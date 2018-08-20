3269270c4237302613eb8825ebd87e3d727281d4
xavier.dutoit <xavier@tttp.eu>
2018-08-20 12:41:42 +0200

    finally, full matching voterid mepid!

--- notes.md
+++ notes.md
@@ -1,4 +1,51 @@
 state of the data:
 they have been 233 days of plenary (according to published rollcalls)
 185 of these have at least one rollcall
-69 of these have it published in a computer readable format
+69 of these have it published in a computer readable format.
+After a week of exchange, they are published, but in french, not in english, and two are still missing.
+After a bit more exchange, the last two missing ones are added
+
+# I'm not a number
+So it turns out that to, for instance, display MEPs only from a single country, you need to, well, have the vote of the mep, and the country of the said mep.
+
+Sounds easy, right? Surely, the EP does give a badge number or some kind of unique identifier that would be used both on for the vote and for the list of meps by country and filtering the list of votes from the polish MEPs would be a 2 min job?
+
+Not so fast, each mep has some kind of unique number. Actually, they have two, one used everywhere (eg list by country) and one for the votes, and they aren't matching. So it's getting slightly more complicated, you need first to build a correspondence between the "regular" identifier and the "vote only" id for each of the MEPs. Surely, it's something that the EP has and is using daily, surely?
+
+Match by name, the hard way
+Match on lastname, attendnance (that use the normal id) and vote (the secret id)
+
+what's in a name? (The Earl of) Dartmouth
+
+On the rollcall,
+369:Keller Jan,6326
+370:Keller Ska,5908
+
+ag keller data/rollcall_name.csv 
+369:Keller Jan,6326
+370:Keller Ska,5908
+xavier@kessel:/var/www/mepwatch/parlparse$ ag keller data/attendance_name.csv 
+355:Jan Keller,124695
+732:Ska Keller,96734
+
+
+
+matching rollcalls and meps information is a nightmare, eg
+got married?
+6605,124993,Valero
+6605,124993,Ceballos
+
+5173,23868,Flašíková Beňová
+5173,23868,Beňová
+6638,125032,Valcárcel
+6638,125032,Valcárcel Siso
+
+matching on name with:
+6677,Rodrigues Liliana,for,S&D,92926
+6371,Rodrigues Maria João,for,S&D,92926
+6649,Rodríguez-Piñero Fernández,for,S&D,92926
+6357,Rozière,for,S&D,92926
+
+and matching on names. What is first or last? with 
+Martin Dominique or Simon Peter
+
--- qa.sh
+++ qa.sh
@@ -2,10 +2,35 @@ q 'select mep,mepid from data/mep_rollcall.csv group by mep' -d, -H -O > data/ro
 q 'select name,id from data/mep_attendance.csv group by name' -d, -H -O > data/attendance_name.csv
 
 q "select mepid,id,name from data/rollcall_name.csv r join data/attendance_name.csv a where r.mep=a.name" -d, -H -O > data/mepidmatch.csv
+#deal with Jan Keller -> Keller Jan mapping
+q "select substr(name, 1, instr(name,' ') -1) first, substr(name, instr(name,' ') +1) last, name, id from data/attendance_name.csv where instr(name,' ') > 0" -d, -H -O > tmp/attendance_splitname.csv
+q "select mepid,id,name from data/rollcall_name.csv r join tmp/attendance_splitname.csv a where r.mep=last || ' ' || first" -d, -H >> data/mepidmatch.csv
+## prepare for manual fixing
+q 'select * from data/attendance_name.csv where id in (124737)' -d, -H -O > data/attendance_manualmatch.csv
+
+#beware, contains duplicates 
+
+## Quality and verifications
+# list of all meps that voted
+q 'select mepid,mep,group from data/mep_rollcall.csv group by mep' -d, -H -O > data/mep_voting.csv
+
+# missing mapping
+q 'select v.* from data/mep_voting.csv v left join data/meps.csv on mepid=voteid left join data/attendance_splitname.csv on  mep=first where voteid is null' -d, -H -O > tmp/vote_unmatched.csv
+
+## add the missing ones?
+q 'select mepid,id,name from tmp/vote_unmatched.csv v join tmp/attendance_splitname.csv a on v.mep=a.last' -d, -H >> data/mepidmatch.csv
+
+## are we missing a few still?
+q 'select v.* from tmp/vote_unmatched.csv v left join tmp/attendance_splitname.csv a on v.mep=a.last where id is null' -d, -H
+
+#lets catch them
+q "select mepid,id,name from  data/attendance_manualmatch.csv m left join tmp/vote_unmatched.csv v on replace(name,rtrim(name,replace(name,' ','')), '')= substr(mep, 1, instr(mep,' ') -1)" -d, -H >> data/mepidmatch.csv
+
 
 #list of meps with voteid
-q 'select m.*, id as voteid from /var/www/ep/data/meps.all.csv m join data/mepidmatch.csv on epid=id' -d, -H -O > data/meps.csv
+q 'select distinct m.*, mepid as voteid from /var/www/ep/data/meps.all.csv m join data/mepidmatch.csv on epid=id' -d, -H -O > data/meps.csv
+
 
 #q 'select a.id mepid,mepid as voteid,mep as name from data/mep_rollcall.csv r join data/mep_attendance.csv a on a.name=r.mep group by a.name' -d, -H -O > data/mepidmatch.csv
 # check if all the rollcalls are published as xml
-#q 'select reference as xml_rollcall_missing from data/rollcall.csv where extensions like "%xml%"' -d, -H -O
+#q 'select reference as xml_rollcall_missing from data/rollcall.csv where extensions notnot  like "%xml%"' -d, -H -O
