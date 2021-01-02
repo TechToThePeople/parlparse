003a260a39c3e404754988c3d47a07e164bf0d89
Xavier <github@tttp.eu>
2021-01-02 17:16:48 +0100

    works fairly ok

--- csv.js
+++ csv.js
@@ -6,36 +6,79 @@ const db = require("./lib/db.js");
 const fs = require("fs");
 const { format } = require("@fast-csv/format");
 
-const file = "./data/item_rollcall.csv";
-const head = "identifier,date,report,desc,title,for,against,abstention".split(
-  ","
-);
-const writeStream = fs.createWriteStream(file);
-const csvStream = format({ headers: head });
-csvStream.pipe(writeStream).on("finish", () => {
-  console.log("fini");
+const rollcall = () => {
+  console.log("aaa");
+  return new Promise(async (resolve, reject) => {
+    const file = "./data/item_rollcall.csv";
+    const head = "identifier,date,report,desc,title,for,against,abstention".split(
+      ","
+    );
+    const writeStream = fs.createWriteStream(file);
+    const csvStream = format({ headers: head });
+    csvStream.pipe(writeStream).on("finish", () => {
+      console.log("fini rollcall");
+      resolve();
+      //    process.exit();
+    });
+
+    const rcv = await db
+      .select(
+        "id as identifier",
+        "date",
+        "for",
+        "against",
+        "abstention",
+        "name as title",
+        "ref as report"
+      )
+      .from("rollcalls");
+    rcv.forEach((r) => {
+      csvStream.write(r);
+    });
+    //console.log(rcv);
+    console.log("read");
+    csvStream.end();
+
+    //csvStream.pipe(process.stdout).on("end", () => process.exit());
+  });
+};
+
+const textTabled = () => {
+  return new Promise(async (resolve, reject) => {
+    const file = "./data/text_tabled.csv";
+    const head = "date,reference,type,title,rapporteur,committee,intra,oeil,doc".split(
+      ","
+    );
+    const writeStream = fs.createWriteStream(file);
+    const csvStream = format({ headers: head });
+    csvStream.pipe(writeStream).on("finish", () => {
+      console.log("end");
+      resolve();
+      //    process.exit();
+    });
+
+    const rcv = await db
+      .select("date", "title", "reference", "committee", "url as oeil")
+      .from("reports");
+    rcv.forEach((r) => {
+      console.log(r);
+      csvStream.write(r);
+    });
+    //console.log(rcv);
+    csvStream.end();
+
+    //csvStream.pipe(process.stdout).on("end", () => process.exit());
+  });
+};
+
+Promise.all([textTabled(), rollcall()]).then((values) => {
+  console.log("done", values);
   process.exit();
 });
 
-(async function () {
-  const rcv = await db
-    .select(
-      "id as identifier",
-      "date",
-      "for",
-      "against",
-      "abstention",
-      "name as title",
-      "ref as report"
-    )
-    .from("rollcalls");
-  rcv.forEach((r) => {
-    csvStream.write(r);
-  });
-  //console.log(rcv);
-  console.log("read");
-  csvStream.end();
-  //csvStream.pipe(process.stdout).on("end", () => process.exit());
+/*
+(async function() {
+  const c = await textTabled ();
+  const d = await rollcall ();
 })();
-
-console.log("process");
+*/
--- report.js
+++ report.js
@@ -1,3 +1,32 @@
-//https://www.europarl.europa.eu/doceo/document/A-9-2020-0267_EN.html
-//https://oeil.secure.europarl.europa.eu/oeil/popups/printresultlist.xml?q=documentEP:D-B9-0411/2020
-//https://oeil.secure.europarl.europa.eu/oeil/popups/printresultlist.xml?q=documentEP:D-RC-B9-0006/2019
+/*
+date,code,status,reference,name,baseurl,extensions
+019-04-18,RCV,final,P8_PV(2019)04-18(RCV),"Procès-verbal- Votes par appel nominal - Jeudi, 18 avril 2019",http://www.europarl.europa.eu/RegData/seance_pleniere/proces_verbal/2019/04-18/liste_presence/P8_PV(2019)04-18(RCV)_XC,pdf|xml|docx
+*/
+
+//https://www.europarl.europa.eu/RegData/seance_pleniere/proces_verbal/yyyy/mm-dd/liste_presence/Pl_PV(yyyy)mm-dd(RCV)_XC.xml
+
+const https = require("https");
+const report = require("./lib/report.js");
+const db = require("./lib/db.js");
+
+let ref = "";
+if (process.argv[2]) {
+  ref = process.argv[2];
+} else {
+  console.log("processing all reports");
+}
+
+(async function run() {
+  //  await init();
+  const start = new Date();
+  console.log(ref);
+  if (!ref) {
+    r = await report.fromRoll();
+  } else {
+    r = await report.process(ref);
+  }
+  console.info("Execution time: %dms", new Date() - start);
+  //  db.destroy();
+})();
+
+console.log("finished");
