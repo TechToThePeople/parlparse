a401e72fd330fd33cdcad1eaf890d6d7c0994ae3
Xavier <github@tttp.eu>
2021-01-05 00:52:49 +0100

    have params to process one or multiple plenaries

--- plenary.js
+++ plenary.js
@@ -6,7 +6,6 @@ date,code,status,reference,name,baseurl,extensions
 //https://www.europarl.europa.eu/RegData/seance_pleniere/proces_verbal/yyyy/mm-dd/liste_presence/Pl_PV(yyyy)mm-dd(RCV)_XC.xml
 
 const https = require("https");
-const fs = require("fs");
 const downloadFile = require("./lib/download.js");
 const roll = require("./lib/rollcall.js");
 const { init } = require("./lib/mep.js");
@@ -17,33 +16,83 @@ const { format } = require("@fast-csv/format");
 const head = "date,code,status,reference,name,baseurl,extensions".split(",");
 const csvStream = format({ headers: head });
 
-let writeStream = fs.createWriteStream("./data/rollcall.csv");
-//csvStream.pipe(process.stdout).on("end", () => process.exit());
-csvStream.pipe(writeStream).on("end", () => process.exit());
-
-//download (()=>(console.log('done')));
-
 let date = null;
-if (process.argv[2]) date = process.argv[2];
-else date = new Date().toISOString().substring(0, 10);
-const d = date.split("-");
-if (d.length !== 3) {
-  console.error("can't parse the date " + date);
-  process.exit(1);
-}
-const name = "P9_PV(" + d[0] + ")" + d[1] + "-" + d[2] + "(RCV)_XC";
-const url =
-  "https://www.europarl.europa.eu/RegData/seance_pleniere/proces_verbal/" +
-  d[0] +
-  "/" +
-  d[1] +
-  "-" +
-  d[2] +
-  "/liste_presence/" +
-  name;
+let initialised = false;
+let argv = require("minimist")(process.argv.slice(2), {
+  alias: { h: "help", a: "all", f: "force", d: "date" },
+});
+const help = (error = false) => {
+  console.log(argv);
+  error && console.error("parameter missing");
+  console.log("--all -a : process all days or:");
+  console.log(
+    "--date=2020-04-09 -d=2020-04-09 : process a single day (today by default if no date)"
+  );
+  console.log(
+    "--force -d : retry download even if already downloaded and parse it anyway (by default, skip)"
+  );
+  process.exit(error);
+};
 
-(async function run() {
+if (argv.help) {
+  help();
+}
+
+if (argv.all) {
+  console.log("process all days");
+  const start = new Date("2019-07-02"); //start of the 9th term
+
+  const end = new Date();
+
+  all(start, end).then(() => {
+    console.info("Execution time: %dms", new Date() - start);
+    process.exit(1);
+  });
+} else {
+  !argv.date && help(true);
+  argv.date === true
+    ? (date = new Date().toISOString().substring(0, 10))
+    : (date = argv.date);
+  const d = date.split("-");
+  if (d.length !== 3) {
+    console.error("can't parse the date " + date);
+    process.exit(1);
+  }
+  const start = new Date();
+  run(date).then(() => {
+    console.info("Execution time: %dms", new Date() - start);
+    process.exit(1);
+  });
+}
+
+async function all(start, end) {
+  let date = end;
+
+  while (date >= start) {
+    await run(date.toISOString().substring(0, 10));
+    date.setDate(date.getDate() - 1);
+  }
+
+  console.log("finished");
+}
+
+async function run(date) {
+  if (!initialised) {
+    initialised = await init();
+  }
+  const d = date.split("-");
   let plenary = null;
+
+  const name = "P9_PV(" + d[0] + ")" + d[1] + "-" + d[2] + "(RCV)_XC";
+  const url =
+    "https://www.europarl.europa.eu/RegData/seance_pleniere/proces_verbal/" +
+    d[0] +
+    "/" +
+    d[1] +
+    "-" +
+    d[2] +
+    "/liste_presence/" +
+    name;
 
   try {
     plenary = await downloadFile("RCV", url, date);
@@ -53,11 +102,14 @@ const url =
   }
 
   await init();
-  const start = new Date();
   plenary.status = "provisional";
   let r = await db("plenaries").insert(plenary).onConflict("date").ignore();
 
   if (r[0] === 0) {
+    if (!argv.force) {
+      console.log("->skip, add --force to process anyway");
+      return;
+    }
     r = await db("plenaries").select("id").where({ date: plenary.date });
     plenary.id = r[0].id;
   } else {
@@ -65,17 +117,5 @@ const url =
   }
 
   await roll(plenary);
-  console.info("Execution time: %dms", new Date() - start);
   //  db.destroy();
-  csvStream.write({
-    date: date,
-    code: "RCV",
-    status: "temp",
-    reference: name,
-    name: "Roll call",
-    baseurl: url,
-    extensions: "xml",
-  });
-})();
-
-console.log("finished");
+}
